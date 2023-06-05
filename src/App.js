f45810b3cc1c9a678a16a75de04be95b45f73c6a
aya bassel <ayabassell@gmail.com>
2023-06-05 15:24:27 +0300

    Login and registration forms

--- src/App.js
+++ src/App.js
@@ -7,15 +7,50 @@ import { Products } from './components/Products';
 import { ProductDetails } from './components/ProductDetails';
 import { ProductForm } from './components/ProductForm';
 import { NotFound } from './components/NotFound';
+import Login from './components/Login';
+import Register from './components/Register';
+import { createContext, useState } from 'react';
+import Admin_products from './components/Admin_products';
+
+// export let usernameContext = createContext();
 
 function App() {
+  let [isLoggedIn, setIsLoggedIn] = useState(false);
+  let [Username, setUsername] = useState("");
+  let [userCategory, setUserCategory] = useState("");
+  let handelLogin = (user) => {
+    setIsLoggedIn(true);
+    setUsername(user.username);
+    setUserCategory(user.client_admin)
+  }
+
   return (
+    
     <div>
-      <MyNav />
+      {/* <usernameContext.Provider value={Username}> */}
+      {
+        isLoggedIn ?  <MyNav changable={Username} /> : <MyNav changable={"Login"} />
+      }
+      {/* </usernameContext.Provider> */}
+      
+      {/* {
+        userCategory === "client" ? 
+      } */}
+      
+      
       <Routes>
         <Route path='' element={<Home />} />
         <Route path='home' element={<Home />} />
-        <Route path='products' element={<Products />} />
+        <Route path='login' element={<Login onLogin={handelLogin} />} />
+        <Route path='register' element={<Register />} />
+        {
+          isLoggedIn && userCategory === "administrator" ? 
+            <Route path='products' element={<Admin_products />} />
+            : isLoggedIn && userCategory === "client" ?             
+              <Route path='products' element={<Products />} /> 
+              : <Route path='products' element={<Login onLogin={handelLogin} />} />
+
+        }
         <Route path='products/:id' element={<ProductDetails />} />
         <Route path='products/:id/edit' element={<ProductForm />} />
         <Route path='*' element={<NotFound />} />
--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 
-export function MyNav() {
+export function MyNav(props) {
     return (
         <div>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
@@ -10,40 +10,25 @@ export function MyNav() {
                     <Navbar.Brand className="Brand" href="#home">Furnitchy</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="ms-auto" id="left-nav">
+                        <Nav id="left-nav">
                             <NavLink className="nav-link" to="/">Home</NavLink>
+                            <NavLink className="nav-link" to="/about">About</NavLink>
                             <NavLink className="nav-link" to="/products">Products</NavLink>
-                            <NavLink className="nav-link" to="/products/0/edit">Add Product</NavLink>
                         </Nav>
+                        {props.changable === "Login" ? (
+                            <Nav className="ms-auto text-light">
+                                <NavLink className="nav-link" to="/login">{props.changable}</NavLink>
+                            </Nav>
+                        ) : (
+                                <Nav className="ms-auto text-light">
+                                    <span> {props.changable}</span>
+                                </Nav>
+                        )}
+
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
         </div>
     );
 }
-// import React, { Component } from 'react'
-// import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap"
-// import '../CSS/MyNav.css'
-// import { NavLink } from 'react-router-dom'
-// export class MyNav extends Component {
-//     render() {
-//         return (
-//             <div>
-//                 <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-//                     <Container>
-//                         <Navbar.Brand className="Brand" href="#home">Furnitchy</Navbar.Brand>
-//                         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-//                         <Navbar.Collapse id="responsive-navbar-nav">
-//                             <Nav className="ms-auto" id="left-nav">
-//                                 <NavLink className="nav-link" to="/">Home</NavLink>
-//                                 <NavLink className="nav-link" to="/products">Products</NavLink>
-//                                 <NavLink className="nav-link" to="/products/0/edit">Add Product</NavLink>
-//                                 <NavLink className="nav-link" to="/login">Login</NavLink>
-//                             </Nav>
-//                         </Navbar.Collapse>
-//                     </Container>
-//                 </Navbar>
-//             </div>
-//         )
-//     }
-// }
+
